38c807f6c5f70c358e5ea2fe023e4bb0d0b64a9a
Bryan O'Sullivan <bos@serpentine.com>
2011-09-06 22:37:11 -0700

    Simple UTF-8 decoding.

--- src/Download.hs
+++ src/Download.hs
@@ -1,5 +1,11 @@
 module Download where
 
+import Data.ByteString.Lazy
+import Data.Text.Lazy
+import Data.Text.Lazy.Encoding
 import Network.HTTP.Enumerator
 
-download url = simpleHttp url
+download :: String -> IO Text
+download url = do
+  page <- simpleHttp url
+  return (decodeUtf8 page)
